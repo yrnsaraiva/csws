534bf190b1a412a17aff74e5d4cf577903a27689
Yuran Saraiva <yrnsaraiva03@gmail.com>
2026-09-04 12:02:48 +0200

    updates

--- config/settings.py
+++ config/settings.py
@@ -100,18 +100,26 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-POSTGRES_LOCALLY = True
-if not DEBUG or POSTGRES_LOCALLY:
-    DATABASES['default'] = dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    )
+if not DEBUG:
+    if not DATABASE_URL:
+        raise RuntimeError("DATABASE_URL não configurada em produção")
+
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=False,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
